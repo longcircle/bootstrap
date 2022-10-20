0d170585732743baf39027b23b966ac55746b19d
longcircle <cwg03125@gmail.com>
2022-10-21 05:12:54 +0900

    2022.10.21

--- src/App.js
+++ src/App.js
@@ -4,57 +4,120 @@ import { Swiper, SwiperSlide } from "swiper/react";
 
 // Import Swiper styles
 import "swiper/css";
-import "swiper/css/grid";
 import "swiper/css/pagination";
+import "swiper/css/navigation";
 
 import "./styles.css";
 
 // import required modules
-import { Grid, Pagination } from "swiper";
+import { Pagination, Navigation } from "swiper";
+import styled from "styled-components";
+
+const Div = styled.div`
+  width: 300px;
+  height: 200px;
+`;
 
 export default function App() {
   return (
     <>
       <Swiper
         slidesPerView={3}
-        grid={{
-          rows: 2,
-        }}
         spaceBetween={30}
+        slidesPerGroup={3}
+        loop={true}
+        loopFillGroupWithBlank={true}
         pagination={{
           clickable: true,
         }}
-        modules={[Grid, Pagination]}
+        navigation={true}
+        modules={[Pagination, Navigation]}
         className="mySwiper"
       >
         <SwiperSlide>
-          <img
-            src="https://mp-seoul-image-production-s3.mangoplate.com/keyword_search/meta/pictures/gwjm44q804mbmvji.jpg?fit=around|600:400&crop=600:400;*,*&output-format=jpg&output-quality=80"
-            alt="first"
-          ></img>
+          <Div>
+            <img
+              src="https://mp-seoul-image-production-s3.mangoplate.com/keyword_search/meta/pictures/gwjm44q804mbmvji.jpg?fit=around|600:400&crop=600:400;*,*&output-format=jpg&output-quality=80"
+              alt="인천 송도 맛집 베스트 50곳"
+            />
+          </Div>
+          <Div>
+            <img
+              src="https://mp-seoul-image-production-s3.mangoplate.com/keyword_search/meta/pictures/dkiovygfrwyedw_r.jpg?fit=around|600:400&crop=600:400;*,*&output-format=jpg&output-quality=80"
+              alt="망원동 맛집 베스트 40곳"
+            />
+          </Div>
         </SwiperSlide>
         <SwiperSlide>
-          <img
-            src="https://mp-seoul-image-production-s3.mangoplate.com/keyword_search/meta/pictures/dkiovygfrwyedw_r.jpg?fit=around|600:400&crop=600:400;*,*&output-format=jpg&output-quality=80"
-            alt="first"
-          ></img>
+          <Div>
+            <img
+              src="https://mp-seoul-image-production-s3.mangoplate.com/keyword_search/meta/pictures/xp8tqpzvpnkze1up.jpg?fit=around|600:400&crop=600:400;*,*&output-format=jpg&output-quality=80"
+              alt="육회 맛집 베스트 15곳"
+            />
+          </Div>
+          <Div>
+            <img
+              src="https://mp-seoul-image-production-s3.mangoplate.com/keyword_search/meta/pictures/j32ez3ban9-rxaur.jpg?fit=around|600:400&crop=600:400;*,*&output-format=jpg&output-quality=80"
+              alt="여의도 불꽃축제 맛집 베스트"
+            />
+          </Div>
         </SwiperSlide>
         <SwiperSlide>
-          {" "}
-          <img
-            src="https://mp-seoul-image-production-s3.mangoplate.com/keyword_search/meta/pictures/dkiovygfrwyedw_r.jpg?fit=around|600:400&crop=600:400;*,*&output-format=jpg&output-quality=80"
-            alt="first"
-          ></img>
+          <Div>
+            <img
+              src="https://mp-seoul-image-production-s3.mangoplate.com/keyword_search/meta/pictures/pgzkxfxjdtfhpxrh.jpg?fit=around|600:400&crop=600:400;*,*&output-format=jpg&output-quality=80"
+              alt="서울 디저트 맛집 베스트 30곳"
+            />
+          </Div>
+          <Div>
+            <img
+              src="https://mp-seoul-image-production-s3.mangoplate.com/keyword_search/meta/pictures/zwh3kvnq1aiwzjjy.jpg?fit=around|600:400&crop=600:400;*,*&output-format=jpg&output-quality=80"
+              alt="회기역 주변 맛집 베스트 25곳"
+            />
+          </Div>
         </SwiperSlide>
-        <SwiperSlide>Slide 4</SwiperSlide>
-        <SwiperSlide>Slide 5</SwiperSlide>
-        <SwiperSlide>Slide 6</SwiperSlide>
-        <SwiperSlide>Slide 7</SwiperSlide>
-        <SwiperSlide>Slide 8</SwiperSlide>
-        <SwiperSlide>Slide 9</SwiperSlide>
-        <SwiperSlide>Slide 10</SwiperSlide>
-        <SwiperSlide>Slide 11</SwiperSlide>
-        <SwiperSlide>Slide 12</SwiperSlide>
+        <SwiperSlide>
+          <Div>
+            <img
+              src="https://mp-seoul-image-production-s3.mangoplate.com/keyword_search/meta/pictures/zn-9npqsx9hugpzb.jpg?fit=around|600:400&crop=600:400;*,*&output-format=jpg&output-quality=80"
+              alt="시청 맛집 베스트 25곳"
+            />
+          </Div>
+          <Div>
+            <img
+              src="https://mp-seoul-image-production-s3.mangoplate.com/keyword_search/meta/pictures/bk2rrl18v7kdrjds.jpg?fit=around|600:400&crop=600:400;*,*&output-format=jpg&output-quality=80"
+              alt="책 읽기 좋은 카페 베스트 40곳"
+            />
+          </Div>
+        </SwiperSlide>
+        <SwiperSlide>
+          <Div>
+            <img
+              src="https://mp-seoul-image-production-s3.mangoplate.com/keyword_search/meta/pictures/yollmteq-ktxy6ec.jpg?fit=around|600:400&crop=600:400;*,*&output-format=jpg&output-quality=80"
+              alt="로스터리 카페 베스트 20곳 사진"
+            />
+          </Div>
+          <Div>
+            <img
+              src="https://mp-seoul-image-production-s3.mangoplate.com/keyword_search/meta/pictures/sjfnxiidoh22pp-4.jpg?fit=around|600:400&crop=600:400;*,*&output-format=jpg&output-quality=80"
+              alt="이천/광주/여주 맛집 베스트 20곳"
+            />
+          </Div>
+        </SwiperSlide>
+        <SwiperSlide>
+          <Div>
+            <img
+              src="https://mp-seoul-image-production-s3.mangoplate.com/keyword_search/meta/pictures/of-6ku6surah9hjs.jpg?fit=around|600:400&crop=600:400;*,*&output-format=jpg&output-quality=80"
+              alt="왕십리역 맛집 베스트 30곳"
+            />
+          </Div>
+          <Div>
+            <img
+              src="https://mp-seoul-image-production-s3.mangoplate.com/keyword_search/meta/pictures/xzxz-ozjnlzqugrv.jpg?fit=around|600:400&crop=600:400;*,*&output-format=jpg&output-quality=80"
+              alt="안주가 맛있는 홍대 맛집 베스트 15곳"
+            />
+          </Div>
+        </SwiperSlide>
       </Swiper>
     </>
   );
